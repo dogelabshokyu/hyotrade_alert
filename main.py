df1f2bc0bfa2481e89f77d624e6a7c8be3490a3e
hokyukim <dogelabshokyu@gmail.com>
2020-09-18 08:53:14 +0900

    Commit

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from selenium import webdriver
 import time
+import telegram
+
 driver = webdriver.Chrome("chromedriver/chromedriver.exe")
+
 kevcase_899 = "https://smartstore.naver.com/hyotrade/products/4809552659"
 kevcase_599 = "https://smartstore.naver.com/hyotrade/products/5006125821"
 acat_x1 = "https://smartstore.naver.com/hyotrade/products/5092067009"
 list = [kevcase_599, kevcase_899, acat_x1]
+
 not_yet = '/html/body/div/div/div[3]/div[2]/div[2]/div/div[2]/div[2]/fieldset/div[9]/strong'
+
+telegram_token = ""
+
+bot = telegram.Bot(token=telegram_token)
 
 def check_status(link):
     try:
@@ -17,12 +25,18 @@ def check_status(link):
         else:
             return False
     except:
-        return False
-
+        return None
+def send_msg(link):
+    updates = bot.getUpdates()
+    chat_id = updates[-1].message.chat_id
+    bot.sendMessage(chat_id=chat_id, text=link)
 
 while(True):
     for i in list:
         print(i)
-        print(check_status(i))
-        time.sleep(1)
-    time.sleep(15)
+        status = check_status(i)
+        if status == False:
+            send_msg(i)
+        elif status == None:
+            send_msg("Connection Error")
+    time.sleep(1800)
