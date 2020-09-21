c42ee162e83fc5c66e2cfaef4a646c72baee1375
Hokyu <dogelabshokyu@gmail.com>
2020-09-21 15:29:17 +0900

    Commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 import time
 import telegram
-import random
 
 driver = webdriver.Chrome("chromedriver/chromedriver.exe")
 
@@ -42,23 +41,23 @@ def write_gall(message):
     driver.find_elements_by_xpath('//*[@id="subject"]')[0].send_keys('효트 짭스트 입고 알림')
     time.sleep(1)
     driver.find_elements_by_xpath('//*[@id="write"]/div[1]/fieldset/div[3]/ul/li[1]')[0].click()
-    time.sleep(random.randrange(0, 5))
+    time.sleep(1)
     driver.find_elements_by_xpath('//*[@id="chk_html"]')[0].click()
     source_code = '<p>짭스트 알림</p><p><a href="'+message+'" target="_blank" class="tx-link">'+message+'</a><br></p>'
     driver.find_elements_by_xpath('//*[@id="tx_canvas_source"]')[0].send_keys(source_code)
-    time.sleep(random.randrange(0, 3))
+    time.sleep(1)
     driver.find_elements_by_xpath('//*[@id="chk_html"]')[0].click()
-    time.sleep(random.randrange(0, 3))
+    time.sleep(1)
     driver.find_elements_by_xpath('//*[@id="write"]/div[4]/button[2]')[0].click()
-    time.sleep(15)
+    time.sleep(4)
 while(True):
     for i in list:
         print(i)
-        write_gall(i)
         status = check_status(i)
         if status == False:
             send_msg(i)
+            write_gall(i)
         elif status == None:
             send_msg("Connection Error")
-        time.sleep(30)
-    time.sleep(1800)
+        time.sleep(5)
+    time.sleep(1775)
